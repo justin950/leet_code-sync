1f98a83d94f27fbee0fb7ff45606dc2d7a9f32b9
justin950 <128875085+justin950@users.noreply.github.com>
2026-07-14 11:33:07 +0530

    Time: 20 ms (10.53%) | Memory: 155.9 MB (26.32%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.dart
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.dart
@@ -3,16 +3,15 @@ class Solution {
     Map<int, int> counts = {};
     List<int> result = [];
     
-    // Count frequencies of each number in nums1
+    
     for (int num in nums1) {
       counts[num] = (counts[num] ?? 0) + 1;
     }
     
-    // Check elements of nums2 against the map
     for (int num in nums2) {
       if (counts.containsKey(num) && counts[num]! > 0) {
         result.add(num);
-        counts[num] = counts[num]! - 1; // Decrement the count
+        counts[num] = counts[num]! - 1;
       }
     }
     
